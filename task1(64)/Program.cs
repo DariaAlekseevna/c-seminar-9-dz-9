d32b647339e6306c3aba313501394c69cb70c99a
Daria Alekseevna <dasha.gugnina98@gmail.com>
2022-07-30 08:10:12 +0300

    решение задачи 1

--- task1(64)/Program.cs
+++ task1(64)/Program.cs
@@ -2,4 +2,16 @@
 которая найдёт сумму натуральных элементов в промежутке от M до N.
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30 */
+int sum = 0;
+void SumFunc(int M, int N)
+{
+    if (M > N)
+        return;
+    System.Console.Write(M + " ");
+    sum += M;
+    SumFunc(M + 1, N);
+}
 
+SumFunc(4, 10);
+System.Console.WriteLine();
+System.Console.WriteLine("сумму натуральных элементов в промежутке от M до N равна: " + sum);
